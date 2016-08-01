b2cd83f07221ec5e77f290d6154b3d6dfa03d9c9
PeterJin <i@jxb.name>
2016-08-01 17:47:48 +0800

    判断参数个数

--- UIBase/UIBase/JxbWebVC.m
+++ UIBase/UIBase/JxbWebVC.m
@@ -50,23 +50,26 @@
 
 - (void)operationAction:(NSString *)operationName paramsNum:(NSNumber *)paramsNum, ... {
     JxbWebAction action = self.dicOperationBlock[operationName];
-    NSMutableArray *argsArray = [[NSMutableArray alloc] init];
-    va_list params; //定义一个指向个数可变的参数列表指针;
-    va_start(params,paramsNum);//va_start 得到第一个可变参数地址,
-    id arg;
-    if (paramsNum) {
-        //va_arg 指向下一个参数地址
-        //这里是问题的所在 网上的例子，没有保存第一个参数地址，后边循环，指针将不会在指向第一个参数
-        while( (arg = va_arg(params,id)) )
-        {
-            if ( arg ){
-                [argsArray addObject:arg];
-                if (argsArray.count >= paramsNum.integerValue)
-                    break;
-            }
-        }  
-        //置空  
-        va_end(params);
+    NSMutableArray *argsArray = nil;
+    if (paramsNum.integerValue > 0) {
+        argsArray = [[NSMutableArray alloc] init];
+        va_list params; //定义一个指向个数可变的参数列表指针;
+        va_start(params,paramsNum);//va_start 得到第一个可变参数地址,
+        id arg;
+        if (paramsNum) {
+            //va_arg 指向下一个参数地址
+            //这里是问题的所在 网上的例子，没有保存第一个参数地址，后边循环，指针将不会在指向第一个参数
+            while( (arg = va_arg(params,id)) )
+            {
+                if ( arg ){
+                    [argsArray addObject:arg];
+                    if (argsArray.count >= paramsNum.integerValue)
+                        break;
+                }
+            }  
+            //置空  
+            va_end(params);
+        }
     }
     if (action != NULL) {
         action(operationName, argsArray);
