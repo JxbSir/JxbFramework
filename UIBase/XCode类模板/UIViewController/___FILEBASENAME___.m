3d65cfdf7403eb13a8c60fe435cf20097a7c67a2
PeterJin <i@jxb.name>
2016-08-25 11:16:51 +0800

    添加类模板

---
+++ UIBase/XCode类模板/UIViewController/___FILEBASENAME___.m
@@ -0,0 +1,53 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Life cycle        / 生命周期
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+#pragma mark - Inital UI         / 初始化界面
+- (void)Jxb_InitalUI {
+    
+}
+
+- (void)Jxb_SetLayouts {
+    
+}
+#pragma mark - Loading data      / 加载数据
+- (void)Jxb_Loaddata {
+    
+}
+#pragma mark - UITableViewModel  / 创建TableView模型
+#pragma mark - SystemDelegate    / 系统代理
+#pragma mark - CustomDelegate    / 自定义代理
+#pragma mark - Observer          / KVO监控
+- (void)Jxb_BeginKvo {
+    
+}
+#pragma mark - Enent response    / 事件响应
+#pragma mark - Private methods   / 私有函数
+#pragma mark - Setter and getter / get与set函数
+
+@end
